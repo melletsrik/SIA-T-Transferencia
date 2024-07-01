ff173143799fe58317f1ccc9d212d52018b30ae1
Cyberialyrc <percy.lujan@ucsm.edu.pe>
2024-06-30 19:24:44 -0500

    Menu seleccionar cuentas tranferencia_otra

--- Interfaces/transferencia_otra.py
+++ Interfaces/transferencia_otra.py
@@ -11,6 +11,22 @@ class TransferenciaOtra(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        conn = connect()
+        cur = conn.cursor()
+        cur.execute("SELECT nro_cuenta FROM mae_cuenta WHERE id_cliente = %s", (self.id_cliente,))
+        cuentas = cur.fetchall()
+        cur.close()
+        conn.close()
+        
+        # Extraer los números de cuenta en una lista
+        self.cuentas = [cuenta[0] for cuenta in cuentas]
+
+        tk.Label(self, text="Cuenta Origen:").pack(pady=10)
+        self.cuenta_origen_var = tk.StringVar(self)
+        self.cuenta_origen_var.set(self.cuentas[0])  # Establecer el primer valor por defecto
+        self.cuenta_origen_menu = tk.OptionMenu(self, self.cuenta_origen_var, *self.cuentas)
+        self.cuenta_origen_menu.pack(pady=5)
+
         tk.Label(self, text="Cuenta Destino:").pack(pady=10)
         self.cuenta_destino_entry = tk.Entry(self)
         self.cuenta_destino_entry.pack(pady=5)
@@ -27,62 +43,75 @@ class TransferenciaOtra(tk.Frame):
         self.master.switch_frame(Menu, self.id_cliente)
 
     def confirmar(self):
+        cuenta_origen = self.cuenta_origen_var.get()
         cuenta_destino = self.cuenta_destino_entry.get()
         monto = self.monto_entry.get()
 
         if cuenta_destino and monto:
-            conn = connect()
-            cursor = conn.cursor()
-            try:
-                cursor.execute("SELECT nro_cuenta FROM mae_cuenta WHERE id_cliente = %s", (self.id_cliente,))
-                cuentas_origen = cursor.fetchall()  # Obtenemos todas las cuentas del cliente
-                try:
-                    monto = float(monto)
-                except ValueError:
-                    messagebox.showerror("Error", "El monto debe ser un número")
-                    return
+            cuenta_origen = self.cuenta_origen_var.get()
+        cuenta_destino = self.cuenta_destino_entry.get()
+        monto = self.monto_entry.get()
 
-                # Validar que el monto sea mayor que cero
-                if monto <= 0:
-                    messagebox.showerror("Error", "El monto debe ser mayor que cero")
-                    return
-                
-                
-                # Obtener todas las cuentas del cliente
-                cursor.execute("SELECT nro_cuenta, saldo_actual FROM mae_cuenta WHERE id_cliente = %s", (self.id_cliente,))
-                cuentas_origen = cursor.fetchall()
-                if not cuentas_origen:
-                    messagebox.showerror("Error", "No se encontraron cuentas para el cliente")
-                    return
-
-                cuenta_origen, saldo_actual = cuentas_origen[0]  # Seleccionamos la primera cuenta del cliente como ejemplo
-
-                # Validar que el monto no exceda el saldo actual de la cuenta origen
-                if monto > saldo_actual:
-                    messagebox.showerror("Error", "El monto excede el saldo actual de la cuenta origen")
-                    return
-                
-                if cuentas_origen:
-                    cuenta_origen = cuentas_origen[0][0]  # Seleccionamos la primera cuenta del cliente como ejemplo
-                    cursor.execute("""
-                        SELECT mae_persona.nombre, mae_persona.apellido 
-                        FROM mae_persona
-                        JOIN mae_cliente ON mae_persona.id_persona = mae_cliente.id_cliente
-                        JOIN mae_cuenta ON mae_cliente.id_cliente = mae_cuenta.id_cliente
-                        WHERE mae_cuenta.nro_cuenta = %s
-                    """, (cuenta_destino,))
-                    destino = cursor.fetchone()
-
-                    if destino:
-                        nombre_destino = f"{destino[0]} {destino[1]}"
-                        self.master.switch_frame(Confirmacion, self.id_cliente, cuenta_origen, cuenta_destino, monto, "otra", nombre_destino)
-                    else:
-                        messagebox.showerror("Error", "Cuenta destino no encontrada")
-                else:
-                    messagebox.showerror("Error", "No se encontraron cuentas para el cliente")
-            except Exception as e:
-                messagebox.showerror("Error", f"Ocurrió un error: {e}")
-            finally:
-                conn.close()
-        else:
+        if not (cuenta_origen and cuenta_destino and monto):
             messagebox.showerror("Error", "Por favor, complete todos los campos")
+            return
+
+        if cuenta_origen == cuenta_destino:
+            messagebox.showerror("Error", "La cuenta origen y la cuenta destino no pueden ser iguales")
+            return
+
+        try:
+            monto = float(monto)
+        except ValueError:
+            messagebox.showerror("Error", "El monto debe ser un número")
+            return
+
+        if monto <= 0:
+            messagebox.showerror("Error", "El monto debe ser mayor que cero")
+            return
+
+        conn = connect()
+        cur = conn.cursor()
+        try:
+            # Verificar si la cuenta destino existe
+            cur.execute("SELECT nro_cuenta FROM mae_cuenta WHERE nro_cuenta = %s", (cuenta_destino,))
+            result_destino = cur.fetchone()
+
+            if result_destino is None:
+                messagebox.showerror("Error", "La cuenta destino no existe")
+                return
+
+            # Obtener el saldo actual de la cuenta origen
+            cur.execute("SELECT saldo_actual FROM mae_cuenta WHERE nro_cuenta = %s", (cuenta_origen,))
+            result_origen = cur.fetchone()
+
+            if result_origen is None:
+                messagebox.showerror("Error", "La cuenta origen no existe")
+                return
+
+            saldo_actual = result_origen[0]
+
+            if monto > saldo_actual:
+                messagebox.showerror("Error", "El monto excede el saldo actual de la cuenta origen")
+                return
+
+            # Obtener el nombre del titular de la cuenta destino
+            cur.execute("""
+                SELECT mae_persona.nombre, mae_persona.apellido 
+                FROM mae_persona
+                JOIN mae_cliente ON mae_persona.id_persona = mae_cliente.id_cliente
+                JOIN mae_cuenta ON mae_cliente.id_cliente = mae_cuenta.id_cliente
+                WHERE mae_cuenta.nro_cuenta = %s
+            """, (cuenta_destino,))
+            destino = cur.fetchone()
+
+            if destino:
+                nombre_destino = f"{destino[0]} {destino[1]}"
+                self.master.switch_frame(Confirmacion, self.id_cliente, cuenta_origen, cuenta_destino, monto, "otra", nombre_destino)
+            else:
+                messagebox.showerror("Error", "No se pudo obtener el nombre del titular de la cuenta destino")
+        except Exception as e:
+            messagebox.showerror("Error", f"Ocurrió un error: {e}")
+        finally:
+            cur.close()
+            conn.close()
